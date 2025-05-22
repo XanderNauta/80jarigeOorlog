973562f9a51d598764bd812c8f3dce9355727b4e
xande <s1148351@student.hsleiden.nl>
2025-05-22 14:10:25 +0200

    ADD opstand page

---
+++ 80jarigeoorlog/src/views/OpstandView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import DargGame from "@/components/DargGame.vue";
+</script>
+
+<template>
+  <div>
+    <DargGame/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
